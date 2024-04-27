2cbe10fc342f93985baf55e638c156b8068f0d2b
MateiSR <MateiSR@proton.me>
2024-04-27 16:51:52 +0300

    Update nvim/lazy plugins

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,71 +1,73 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"mfussenegger/nvim-dap",
-		},
-		opts = {
-			handlers = {},
-			ensure_installed = {
-				"codelldb",
-			},
-		},
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-		event = "VeryLazy",
-		dependecies = {
-			"mfussenegger/nvim-dap",
-		},
-		config = function()
-			local dap, dapui = require("dap"), require("dapui")
-			dapui.setup()
-			-- Setup C/C++/Rust DAP
-			dap.adapters.lldb = {
-				type = "executable",
-				command = "/home/matei/.local/share/nvim/mason/bin/codelldb", -- Installed through mason
-				name = "lldb",
-			}
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+      ensure_installed = {
+        "codelldb",
+      },
+    },
+  },
+  { "nvim-neotest/nvim-nio" },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependecies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+      -- Setup C/C++/Rust DAP
+      dap.adapters.lldb = {
+        type = "executable",
+        command = "/home/matei/.local/share/nvim/mason/bin/codelldb", -- Installed through mason
+        name = "lldb",
+      }
 
-			dap.configurations.cpp = {
-				{
-					name = "Launch",
-					type = "lldb",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-					end,
-					cwd = "${workspaceFolder}",
-					stopOnEntry = false,
-					args = {},
-				},
-			}
+      dap.configurations.cpp = {
+        {
+          name = "Launch",
+          type = "lldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+          args = {},
+        },
+      }
 
-			dap.configurations.c = dap.configurations.cpp
-			dap.configurations.rust = dap.configurations.cpp
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
-			vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-			vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-			vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-			vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-		end,
-	},
+      vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+      vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+      vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+      vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+    end,
+  },
 }
