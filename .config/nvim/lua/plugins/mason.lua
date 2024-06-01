e065265b81920a8efd76cee48e3636265905b0d4
MateiSR <MateiSR@proton.me>
2024-06-02 00:52:17 +0300

    chore(nvim-lsp): reformat plugin config
    combine servers in a shared array,
    add bashls

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,46 +1,41 @@
+-- Define the plugins for the mason plugin manager
+local servers = {
+	"lua_ls",
+	"tsserver",
+	"clangd",
+	"pyright",
+	"rust_analyzer",
+	"biome",
+	"html",
+	"bashls",
+}
+
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "clangd",
-          "pyright",
-          "rust_analyzer",
-          "biome",
-          "html",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local servers = {
-        "lua_ls",
-        "tsserver",
-        "clangd",
-        "pyright",
-        "rust_analyzer",
-        "biome",
-        "html",
-      }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = servers,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({})
-      end
+			for _, server in ipairs(servers) do
+				lspconfig[server].setup({})
+			end
 
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
